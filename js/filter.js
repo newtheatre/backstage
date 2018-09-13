35512bf48e9e5b9298c7be295d6619acf45fbd2f
Sam Osborne <sam.tosborne@googlemail.com>
2018-09-13 21:45:30 +0100

    Filter causes reflow
    Now hides the 'li' rather than its contents (oops)

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,16 @@
 $(document).ready(function () { // Filter buttons
 	$('.category-item').on('click', function() {
-	  if ($(this).hasClass('active')) {
-	    $('.set-item').removeClass('d-none');
-	    $(this).removeClass('active');
+	  if ($(this).hasClass('active')) { // If filter applied already
+	    $('.set-item').parent().removeClass('d-none'); // Make others visible
+	    $(this).removeClass('active'); // Deactivate filter 
 	    return;
 	  } 
 	  else {
-	    var $el = $('.set-type-' + this.id).removeClass('d-none');
-	    $('.set-item').not($el).addClass('d-none');
-	    console.log('Done');
+	    var $match = $('.set-type-' + this.id)
+	    $($match).parent().removeClass('d-none'); // Make any hidden matches visible
+	    $('.set-item').not($match).parent('li').addClass('d-none'); // Hide the rest
 	  }
-	  $('.category-item').removeClass('active');
+	  $('.category-item').removeClass('active'); 
 	  $(this).addClass('active');
 	})
 });
